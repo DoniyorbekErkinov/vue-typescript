cfe13b9aef2c788cf85d654e68e71aabdd2d8264
doniyor <erkinovdoniyor03@gmail.com>
2023-01-03 12:37:48 +0500

    commit

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,10 @@ export const useStore = defineStore('Store', {
     state: (): StateShape => ({
         list: data
     }),
-    getters: {},
+    getters: {
+        getLength: (state) => {
+            state.list.length
+        }
+    },
     actions: {}
 })
